f69e152a604f68e758e6e8beac883d1fcfe328a4
Dipankar Dutta <deepankar.dutta1@gmail.com>
2026-05-17 17:37:13 +0530

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+        if(strs.length==0) return "";    
+        StringBuilder sb=new StringBuilder();
+
+        // find the shortest length string 
+        int len=strs[0].length();
+        // as i =0 already used in initialization
+        for(int i=1;i<strs.length;i++){
+            len=Math.min(len,strs[i].length());
+        }
+
+        for(int i=0;i<len;i++){
+            char s=strs[0].charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(s!=strs[j].charAt(i)){
+                    return sb.toString();
+                }
+            }
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+}
