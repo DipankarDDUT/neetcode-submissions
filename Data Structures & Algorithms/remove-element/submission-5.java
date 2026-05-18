877562ca4a3ae5b763d5f9810ef3c4d33d235424
Dipankar Dutta <deepankar.dutta1@gmail.com>
2026-05-18 18:15:18 +0530

    Add: remove-element - submission-5

---
+++ Data Structures & Algorithms/remove-element/submission-5.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void swap(int[] nums,int i,int j){
+        if(i!=j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+    }
+    public int removeElement(int[] nums, int val) {
+        
+        int n=nums.length-1;
+        int i=0;
+        int j=n;
+        //current index + (nums.length-k+1) number of items eqaul to val
+        while(i<=j){
+
+                if(nums[i]==val){
+                    swap(nums,i,j);
+                    j--;
+                }else{
+                    i++;
+                }
+
+                
+        }
+
+        // i will point to index on first val index after that all index is val
+        // as out put is 1 based index so remove -1 
+        // return i-1;
+        return i;
+    }
+}
