48df6fda854e24cf12bd8bb1630408da8a11ffff
Dipankar Dutta <deepankar.dutta1@gmail.com>
2026-05-22 06:37:18 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-5

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        
+        // brute for each find the largest on right side and assign the value to result
+        // for each element
+        // so time O(n sqaure)
+
+        int n=arr.length-1;
+        int curMax=-1;
+        for(int i=n;i>=0;i--){
+            // always assign the right side current max irrespective
+            // its larger than current or not 
+            int current=arr[i];
+            arr[i]=curMax;
+            curMax=Math.max(current,curMax);
+        }
+
+        return arr;
+
+    }
+}
